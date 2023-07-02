edeb1d534dd74ce8f448817362bc11ee0159f3e7
rmdy <rmdy@infrastructures-MacBook-Pro.local>
2023-07-02 10:58:02 +0800

    Updates

--- static/chunks/746.eb54eaf2d5ad7dc4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[746],{3746:function(a){a.exports={style:{fontFamily:"'__Montserrat_2a7d8f', '__Montserrat_Fallback_2a7d8f'",fontStyle:"normal"},className:"__className_2a7d8f",variable:"__variable_2a7d8f"}}}]);
